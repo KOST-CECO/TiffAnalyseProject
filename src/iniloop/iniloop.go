336c9feac2da6f1b0047af44d3b2a79d8397f064
Martin Kaiser <martin.kaiser@kost.admin.ch>
2016-03-31 12:42:05 +0200

    iniloop erweitert über mehrer folder Ebenen

--- src/iniloop/iniloop.go
+++ src/iniloop/iniloop.go
@@ -3,20 +3,35 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
-	doloop(("./"))
+	// read initial parameter from command line
+	if len(os.Args) == 2 {
+		fi, err := os.Stat(os.Args[1])
+		if (err != nil) && fi.IsDir() {
+			doloop(fi.Name() + "/")
+		} else {
+			fmt.Println("error: " + os.Args[1] + " is not a valid folder name")
+		}
+	} else {
+		fmt.Println("usage: " + os.Args[0] + " [folder]")
+	}
 }
 
 func doloop(dir string) {
 	files, _ := ioutil.ReadDir(dir)
 
 	for _, f := range files {
-		if f.IsDir() {
-			fmt.Println("DIR")
-		} else {
-			fmt.Println(f.Name())
+		// ignore hidden files and folder
+		if (f.Name()[0:1]) != "." {
+			if f.IsDir() {
+				doloop(dir + f.Name() + "/")
+			} else {
+				fmt.Println(dir + f.Name())
+			}
+
 		}
 
 	}
