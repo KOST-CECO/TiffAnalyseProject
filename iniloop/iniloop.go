52f6981731a7c3090eac549248905a96a4420e28
Martin Kaiser <martin.kaiser@kost.admin.ch>
2016-04-14 16:32:14 +0200

    Anfangszähler für KEYFILE eingefügt

--- iniloop/iniloop.go
+++ iniloop/iniloop.go
@@ -11,6 +11,8 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var keyCounter int32 = 0 // holds the latest id used in namefile and keyfile
+
 func main() {
 	// read initial parameter from command line
 	if len(os.Args) != 3 {
@@ -21,33 +23,36 @@ func main() {
 	// check for valid folder
 	fo, foerr := os.Stat(os.Args[1])
 	if foerr != nil {
-		log.Fatal("err: " + os.Args[1] + " is not a valid folder name")
+		log.Fatal(os.Args[1] + " is not a valid folder name")
 	}
 	if !fo.IsDir() {
-		log.Fatal("err: " + os.Args[1] + " is not a folder")
+		log.Fatal(os.Args[1] + " is not a folder")
 	}
 
 	// check for valid file
 	fi, fierr := os.Stat(os.Args[2])
 	if fierr != nil {
-		log.Fatal("err: " + os.Args[2] + " is not a valid database name")
+		log.Fatal(os.Args[2] + " is not a valid database name")
 	}
 	if !fi.Mode().IsRegular() {
-		log.Fatal("err: " + os.Args[2] + " is not a database file")
+		log.Fatal(os.Args[2] + " is not a database file")
 	}
 
 	// check for valid sqlite3 database
 	db, err := sql.Open("sqlite3", os.Args[2])
 	if err != nil {
-		log.Fatal("err: " + os.Args[2] + " is not a sqlite3 database")
+		log.Fatal(os.Args[2] + " is not a sqlite3 database")
 	}
 	defer db.Close()
-	// select max(id) from keyfile
-	sqlStmt := `select count(id) from keyfile;`
-	_, err = db.Exec(sqlStmt)
+	rows, err := db.Query("SELECT MAX(id) FROM keyfile")
 	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		log.Fatal("err: " + os.Args[2] + " is not valide sqlite3 database")
+		log.Print(err)
+		log.Fatal(os.Args[2] + " is not valide TAP database")
+	}
+	defer rows.Close()
+	for rows.Next() {
+		rows.Scan(&keyCounter)
+		fmt.Println(keyCounter)
 	}
 
 	// run folder walk
