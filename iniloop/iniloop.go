361c2663fb038025f704e9d083f0d71a092a716b
mkaiser56 <m.kaiser@access.ch>
2016-04-17 16:10:22 +0200

    Schreiben von NAMEFILE implementiert

--- iniloop/iniloop.go
+++ iniloop/iniloop.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 
-	"github.com/KOST-CECO/TiffAnalyseProject/looputil"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+// global variable definitions
+var KeyCounter int32 = 0 // holds the latest id used in namefile and keyfile
+
 func main() {
+	// for test purpose only
+	cmd := exec.Command("C:/Tools/PCUnixUtils/cp.exe", "tab_backup.db", "tap.db")
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// read initial parameter from command line
 	if len(os.Args) != 3 {
 		fmt.Println("usage: " + os.Args[0] + " [options] folder database")
@@ -35,14 +48,73 @@ func main() {
 	}
 
 	// check for valid sqlite3 database and open database
-	looputil.CheckDb(os.Args[2])
+	TapDb, err := sql.Open("sqlite3", os.Args[2])
+	if err != nil {
+		log.Fatal(os.Args[2] + " is not a sqlite3 database")
+	}
+	defer TapDb.Close()
+
+	rows, err := TapDb.Query("SELECT MAX(id) FROM keyfile")
+	if err != nil {
+		log.Print(err)
+		log.Fatal(os.Args[2] + " is not valide TAP database")
+	}
+	defer rows.Close()
+	for rows.Next() {
+		rows.Scan(&KeyCounter)
+		fmt.Println(KeyCounter)
+	}
 
 	// run folder walk
 	path, err := filepath.Abs(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
-	looputil.Procloop(path + string(os.PathSeparator))
+	procloop(path+string(os.PathSeparator), TapDb)
 	os.Exit(0)
+}
 
+// read all files in actual folder and recurse through subsequent folders
+func procloop(dir string, TapDb *sql.DB) {
+	files, _ := ioutil.ReadDir(dir)
+
+	for _, f := range files {
+		// ignore hidden files and folder
+		if (f.Name()[0:1]) != "." {
+			if f.IsDir() {
+				// subsequent folder detected
+				procloop(dir+f.Name()+string(os.PathSeparator), TapDb)
+			} else {
+				// process file
+				// fmt.Println(dir + f.Name())
+				procfile(dir, f, TapDb)
+			}
+
+		}
+
+	}
+}
+
+// write FileInfo in database namefile and keyfile
+func procfile(dir string, file os.FileInfo, TapDb *sql.DB) {
+	KeyCounter = KeyCounter + 1
+
+	tx, err := TapDb.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO namefile (id, serverame, filepath, filename) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(KeyCounter, filepath.VolumeName(dir), dir, file.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	tx.Commit()
+
+	fmt.Println(dir + file.Name())
 }
