1246629dd55ebf1f268ca119f0dcf4ad0eb60827
mkaiser56 <m.kaiser@access.ch>
2016-06-14 16:22:11 +0200

    hello world eingefuegt

--- checkMd5/checkMd5.go
+++ checkMd5/checkMd5.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
-
-	"github.com/KOST-CECO/TiffAnalyseProject/util"
+	"io"
+	"os"
 )
 
 func main() {
-	md5, _ := util.ComputeMd5("checkMd5.go")
+	md5, _ := computeMd5("checkMd5.go")
 	fmt.Printf("%x", md5)
+}
+
+// compute md5 of file
+// http://dev.pawelsz.eu/2014/11/google-golang-compute-md5-of-file.html
+func computeMd5(filePath string) ([]byte, error) {
+	var result []byte
+	file, err := os.Open(filePath)
+	if err != nil {
+		return result, err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return result, err
+	}
+
+	return hash.Sum(result), nil
 }
